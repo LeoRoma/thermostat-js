d26507f8b3c9d75ae80dc4a522e04ce212744e08
LeoRoma <xiajtn@gmail.com>
2020-01-08 10:58:10 +0000

    styling thermostat

--- jquery.js
+++ jquery.js
@@ -32,4 +32,19 @@ $(document).ready(function() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.displayUsage())
   };
+
+  $(document).ready(function() {
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp);
+    });
+  });
+  
+  $(document).ready(function() {
+    $('#current-city').change(function() {
+    var city = $('#current-city').val();
+        $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+          $('#current-temperature').text(data.main.temp)
+        });
+    });
+  });
 });
